309f95d1527b3dd2ccfd166d1ca9d8ffa2070358
Binyan Li <lbyxiafei@gmail.com>
2022-05-30 20:12:03 -0700

    Refactor to a codable barebone state.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,27 @@
 <template>
+  <header>
+    <nav>
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
 </template>
 
 <script>
+import { RouterLink} from 'vue-router'
+export default{
+    name: "Header",
+    components:{
+        RouterLink
+    }
+}
 </script>
+
+<style scoped>
+nav {
+  width: 100%;
+  display: inline-block;
+  font-size: 1.3rem;
+  text-align: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <template>
-  <h1>Home</h1>
+  <h1 class="center-screen">Home</h1>
 </template>
 
 <script>
-
+export default{
+  name: "Home"
+}
 </script>
 
+<style scoped>
+@import '../assets/base.css'
+</style>
