29ca2e408f5293fcd1d8a78f3f491471acbb8763
Binyan Li <binyanli@microsoft.com>
2022-08-05 11:08:30 -0700

    Minor.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig, loadEnv } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    // vite config
     define: {
       __APP_ENV__: env.APP_ENV
     },
