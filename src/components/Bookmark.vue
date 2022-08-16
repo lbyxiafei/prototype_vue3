af5707ddf8c5c07b96c6088d5f307710140aa535
Binyan Li <lbyxiafei@gmail.com>
2022-08-15 22:06:59 -0700

    Apply icon sheet.

--- src/components/Bookmark.vue
+++ src/components/Bookmark.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container bg-light">
     <div class="card bg-light text-secondary">
-      <div class="h3 card-title mb-3"><a :href="bookmark.url" target="_blank">{{bookmark.title}}</a></div>
+      <div class="h1 mb-3 text-dark">
+        <i class="bi" :class="[tagIcon]"></i>
+      </div>
       <div class="card-text"><a :href="bookmark.url" target="_blank">{{bookmark.title}}</a></div>
       <div class="container">
-        <i class="fa-regular fa-pen-to-square text-dark" @click="showBookmarkModal = true" />
-        <i class="fa-regular fa-trash-can text-dark" @click="$emit('delete-bookmark', bookmark)" />
+        <i class="fa-regular fa-pen-to-square text-success" @click="showBookmarkModal = true" />
+        <i class="fa-regular fa-trash-can text-secondary" @click="$emit('delete-bookmark', bookmark)" />
       </div>
     </div>
   </div>
@@ -26,6 +28,7 @@ export default{
   name: "Bookmark",
   data(){
     return {
+      tagIcon: this.getTagIcon(),
       showBookmarkModal: false
     }
   },
@@ -36,6 +39,22 @@ export default{
     BookmarkModal
   },
   methods: {
+    getTagIcon(){
+      const tags = new Set();
+      this.bookmark.tags.forEach(tag => {
+        tags.add(tag.name);
+      });
+      if(tags.has("watch")) return "bi-eye";
+      if(tags.has("todo")) return "bi-list-check";
+      if(tags.has("algo")) return "bi-lightning";
+      if(tags.has("cheatsheet")) return "bi-lightbulb";
+      if(tags.has("communication")) return "bi-wechat";
+      if(tags.has("jobs")) return "bi-linkedin";
+      if(tags.has("training")) return "bi-battery-charging";
+      if(tags.has("debug")) return "bi-bug";
+      if(tags.has("cpp")) return "bi-code";
+      return "bi-info-circle";
+    }
   },
   emits: ['save-bookmark', 'delete-bookmark'],
 }
