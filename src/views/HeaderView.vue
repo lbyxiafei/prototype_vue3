853ec8fdddf897af237311a0e539e1eb5c72f61e
Binyan Li <binyanli@microsoft.com>
2022-08-17 23:20:41 -0700

    Check point, 2022-08-17T23:20:41.

--- src/views/HeaderView.vue
+++ src/views/HeaderView.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="d-flex flex-wrap align-items-center justify-content-center py-3 mb-4">
-    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">?
-      <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-    </a>
-
     <div class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
       <RouterLink class="nav-link px-2 link-dark text-primary" to="/">Home</RouterLink>
       <RouterLink class="nav-link px-2 link-dark" to="/bookmarks">Bookmarks</RouterLink>
